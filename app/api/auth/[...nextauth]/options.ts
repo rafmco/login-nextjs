a1ff551eaabf218c37b8df6719c4850e4eb8ba4a
Rafael Machado <rafmco2@hotmail.com>
2024-05-07 16:00:37 -0400

    chore: add github provider em AuthOptions;

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -1,9 +1,14 @@
 import CredentialsProvider from "next-auth/providers/credentials";
+import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
 import { AuthOptions } from "next-auth";
 
 export const authOptions: AuthOptions = {
   providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID as string,
+      clientSecret: process.env.GITHUB_SECRET as string,
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID as string,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
@@ -11,15 +16,15 @@ export const authOptions: AuthOptions = {
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        username: {
-          label: "Username:",
-          type: "text",
-          placeholder: "your-username",
+        email: {
+          label: "Email",
+          type: "email",
+          placeholder: "hello@example.com",
         },
         password: {
-          label: "Password:",
+          label: "",
           type: "password",
-          placeholder: "your-password",
+          placeholder: "Senha",
         },
       },
       // Get Data from database
@@ -28,10 +33,15 @@ export const authOptions: AuthOptions = {
         // This is where you need to retrieve user data
         // to verify with credentials
         // Docs: https://authjs.dev/getting-started/authentication/credentials#credentials-provider
-        const user = { id: "1", name: "Rafael", password: "nextauth" };
+        const user = {
+          id: "1",
+          email: "rafael@dev",
+          name: "Rafael Machado",
+          password: "nextauth",
+        };
 
         if (
-          credentials?.username === user.name &&
+          credentials?.email === user.email &&
           credentials?.password === user.password
         ) {
           // return user object with the their profile data
